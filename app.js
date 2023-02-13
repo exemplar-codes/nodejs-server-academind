e1e0ef52406ae8d6ab943f2e514e2eb33c2380eb
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-14 01:55:56 +0530

    Routing based on method and route

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
+  if (req.url === "/" && req.method === "GET") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Home page</title></head>");
-    res.write("<body><h1>Hello from my Node.js server</h1></body");
+    res.write(
+      `<body>
+			<form action='/message' method='POST'>
+				<input type='text' name='message'
+				placeholder='Enter text and press Enter'
+				size="40px" />
+			</form>
+	   </body`
+    );
     res.write("</html>");
     res.end();
     return;
-  } else if (req.url === "/about") {
+  } else if (req.url === "/message" && req.method === "POST") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>About</title></head>");
-    res.write("<body><h1>I am Sanjar</h1></body");
+    res.write("<head><title>Success</title></head>");
+    res.write("<body><h1>Form submitted!</h1></body");
     res.write("</html>");
     res.end();
     return;
